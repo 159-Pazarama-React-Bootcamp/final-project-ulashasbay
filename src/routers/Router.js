b5b42e620056a837f63b0f72cd503c5a73482477
Ulaş Hasbay <ulashasbay@gmail.com>
2022-01-26 08:05:46 +0300

    ApplicationSuccess page,  protectetRoute added

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -4,11 +4,13 @@ import Admin from "../pages/Admin";
 import ApplicationForm from "../pages/ApplicationForm";
 import AdminApplicationList from "../pages/AdminApplicationList";
 import ApplicationInfo from "../pages/ApplicationInfo";
+import ApplicationSuccessful from "../pages/ApplicationSuccessful";
 import AdminApplicationInfo from "../pages/AdminApplicationInfo";
 import Home from "../pages/Home";
 import ApplicationQuery from "../pages/ApplicationQuery";
 import NotFoundPage from "../pages/NotFoundPage";
 import PrivateRoute from "./PrivateRoute";
+import ProtectedRoute from "./ProtectedRoute";
 import { useSelector } from "react-redux";
 
 function Router() {
@@ -23,13 +25,15 @@ function Router() {
             element={<AdminApplicationInfo />}
           />
         </Route>
-        <Route path="basvuru-basarili" element={<ApplicationInfo />} />
         <Route path={`/basvuru/${appIdValue}`} element={<ApplicationInfo />} />
         <Route element={<PrivateRoute />}>
           <Route
             path="admin/basvuru-listesi"
             element={<AdminApplicationList />}
           />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="basvuru-basarili" element={<ApplicationSuccessful />} />
         </Route>
         <Route path="admin" element={<Admin />} />
         <Route path="basvuru-olustur" element={<ApplicationForm />} />
